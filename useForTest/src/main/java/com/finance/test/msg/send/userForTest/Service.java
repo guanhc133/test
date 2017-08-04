123d3f84f53c0502220d436d2f4d7f372c43b0b7
guanhc <guanhc@oriental-finance.com>
2017-08-04 13:31:37 +0800

    面向切面编程aop实例演示

---
+++ useForTest/src/main/java/com/finance/test/msg/send/userForTest/Service.java
@@ -0,0 +1,19 @@
+package com.finance.test.msg.send.userForTest;
+
+/**
+ * 描述：
+ * <p>
+ * #
+ * </p>
+ * User: guanhc Date: 2017/8/4 ProjectName:test Version:
+ */
+@org.springframework.stereotype.Service
+public class Service {
+    public void doSomethingOne(){
+        System.out.println("业务服务1");
+    }
+
+    public void doSomethingTwo(){
+        System.out.println("业务服务2");
+    }
+}
