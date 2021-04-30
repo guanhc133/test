9cd54feb0324ef73167a81ebf01f358a18bfe148
guanhongcheng <guanhongcheng@tcpjw.com>
2021-04-30 09:13:24 +0800

    添加测试

--- src/test/java/org/example/StateTest.java
+++ src/test/java/org/example/StateTest.java
@@ -13,10 +13,24 @@ public class StateTest {
     @Test
     public void EditingToArraignment() {
         String activityId = "100001";
+        //当前状态
         ActivityService.init(activityId, Status.Editing);
         StateHandler stateHandler = new StateHandler();
+        //调用待变更状态对应的接口 arraignment
         String result = stateHandler.arraignment(activityId, Status.Editing);
         System.out.println("测试结果(编辑中To提审活动)：" + result);
+        System.out.println("活动信息：" + JSON.toJSONString(ActivityService.queryActivityInfo(activityId))
+                + " 状态：" + JSON.toJSONString(ActivityService.queryActivityInfo(activityId).getStatus()
+        ));
+    }
+
+    @Test
+    public void ArraignmentToDoing() {
+        String activityId = "100001";
+        ActivityService.init(activityId, Status.Check);
+        StateHandler stateHandler = new StateHandler();
+        String result = stateHandler.doing(activityId, Status.Check);
+        System.out.println("测试结果(审核中To活动中)：" + result);
         System.out.println("活动信息：" + JSON.toJSONString(ActivityService.queryActivityInfo(activityId))
                 + " 状态：" + JSON.toJSONString(ActivityService.queryActivityInfo(activityId).getStatus()
         ));
